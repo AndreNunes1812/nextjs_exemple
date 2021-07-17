fa0c75e3a90bfc5f9b92f35432d34285a32b135e
Andre Nunes <andre@grupoeureka.com.br>
2021-07-18 00:10:56 +0200

    finaly service api

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -14,8 +14,8 @@ export default function Inicio() {
         <Navegador destino="/jsx" text="JSX" color="crimson"/> 
         <Navegador destino="/navegacao" text="Navegação #01" color="green"/> 
         <Navegador destino="/cliente/sp-2/123" text="Navegação #02" color="blue"/> 
-        
-               
+        <Navegador destino="/estado" text="Componente com Estado" color="#a45b71"/>         
+        <Navegador destino="/integracao_1" text="Integração com API #01" color="#42a9a9"/> 
       </div>
     )
 }
---
+++ src/pages/integracao_1.jsx
@@ -0,0 +1,40 @@
+import { useState } from 'react';
+import Layout from '../components/Layout';
+
+export default function Integracao() {
+
+  const [codigo, setCodigo] = useState(1);
+  const [cliente, setCliente] = useState({});
+  
+
+  async function obterCliente() {
+    /* Exemplo 1 */
+    const resp = await fetch(`http://localhost:3000/api/clientes/${codigo}`);
+    const dados = await resp.json();
+    setCliente(dados);
+
+    /* Exemplo 2 
+      fetch(`http://localhost:3000/api/clientes/${codigo}`)
+      .then(resp => resp.json())
+      .then( dados => setCliente(dados)); 
+    */
+  }
+
+  return (
+    <Layout>
+      <div>
+        <input 
+          type="number" 
+          value={codigo} 
+          onChange={ e => setCodigo(e.target.value)} 
+        />
+        <button onClick={obterCliente} >Obter Cliente</button>
+      </div>
+      <ul>
+        <li>Código: {cliente.id}</li>
+        <li>Nome: {cliente.nome}</li>
+        <li>Email: {cliente.email}</li>        
+      </ul>
+    </Layout>
+  )
+}
